53fee386149eb013118d0bbcb7e245410967e24f
Jason <jkuffler@catalyte.io>
2023-02-01 17:02:15 -0500

    added some projects

--- index.js
+++ index.js
@@ -32,33 +32,12 @@ fetch("https://api.github.com/users/JKuffler")
       : (hireable.innerHTML = `Hireable: ⛔`)
   })
 
-/* PROJECT SECTION  */
-// fetch("https://api.github.com/users/JKuffler/repos")
-//   .then(r => r.json())
-//   .then(repos => {
-//     const projectSection = document.getElementById("projects")
-//     const projectsList = projectSection.querySelector("ul")
-
-//     const sortedRepos = repos.sort((a, z) => z.id - a.id)
-//     // console.log(sortedRepos)
-//     for (let i = 0; i < 10; i++) {
-//       const repos = document.createElement("li")
-//       repos.innerHTML =
-//         `<a href=${sortedRepos[i].html_url} target='_blank' rel="noopener noreferrer">` +
-//         `  ${sortedRepos[i].name}  `.toString().slice(0, 22) +
-//         `|Created| ` +
-//         new Date(`${sortedRepos[i].created_at}`).toDateString() +
-//         `</a>`
-//       projectsList.appendChild(repos)
-//     }
-//   })
-
 /** LINKDin */
 const linkBadge = document.getElementById("link_badge")
 
 linkBadge.innerHTML = `<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="jason-kuffler" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/jason-kuffler?trk=profile-badge">LinkedIn</a></div>
 `
-/** lolz for the copyright footer or whatever sure */
+
 const today = new Date()
 const thisYear = today.getFullYear()
 const footer = document.querySelector("footer")
